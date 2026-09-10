b76518ea60fdf7e290b220c95665b75e3ad92c60
Vishal <99464168+vishal9650936756@users.noreply.github.com>
2026-09-10 23:41:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (44.44%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long power = n;
+
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        double ans = 1.0;
+
+        while (power > 0) {
+            if (power % 2 == 1) {
+                ans *= x;
+            }
+
+            x *= x;
+            power /= 2;
+        }
+
+        return ans;
+    }
+};
